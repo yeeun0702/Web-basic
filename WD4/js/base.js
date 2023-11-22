cbf8bab9d9ba8fc402d1ef8eda18315280c44498
yeeun0702 <kimyeeun0702@naver.com>
2023-11-22 17:11:48 +0900

    WD4

---
+++ WD4/js/base.js
@@ -0,0 +1,5 @@
+function change() {
+    var li = document.getElementById("myli");
+    li.style.color = "skyblue";
+    li.style.fontSize = "25px";
+  }
---
+++ WD4/js/future.js
@@ -0,0 +1,7 @@
+
+function trip() {
+    if (form.answer[0].checked == true) window.location.href = "https://www.gn.go.kr/tour/index.do" ;
+    if (form.answer[1].checked == true) window.location.href = "https://www.visitbusan.net/kr/index.do";
+    if (form.answer[2].checked == true) window.location.href = "https://tour.jeonju.go.kr/index.jeonju#content";
+    if (form.answer[3].checked == true) window.location.href = "https://www.visitjeju.net/kr" ;
+}
